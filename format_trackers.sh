e459281a3ec78da5604721c732234711b3947f70
shniubobo <shniubobo@outlook.com>
2022-10-27 13:08:23 +0800

    Exclude IPs from tracker list

--- format_trackers.sh
+++ format_trackers.sh
@@ -2,10 +2,12 @@
 set -euo pipefail
 
 regex='^[a-z]+:\/\/([^:\/]+)(:[0-9]+)?\/.*$'
+regex_ip='^([0-9]{1,3}\.){3}[0-9]{1,3}$'
 
 format() {
     if [[ "$1" =~ $regex ]]; then
-        echo "${BASH_REMATCH[1]}"
+        domain="${BASH_REMATCH[1]}"
+        [[ "$domain" =~ $regex_ip ]] || echo "$domain"
     fi
 }
 
